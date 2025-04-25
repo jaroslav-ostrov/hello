3ac189e1fab937c023675725fc2cf6d3d49582ed
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-04-25 14:55:45 +0300

    Удалил все коментарии

--- main.c
+++ main.c
@@ -4,11 +4,11 @@
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
     const char* lang = "C";
-     printf("Hello and welcome to %s\n",  lang);
+     
 
     for (int i = 1; i <= 5; i++) {
         // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-         printf( "i = %i" , i  );
+
     }
 
     return 0;
