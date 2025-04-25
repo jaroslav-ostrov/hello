c270a3496dafa0500aa8d481226a700802c267c3
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-04-25 16:57:39 +0300

    array and its bounds

--- main.c
+++ main.c
@@ -2,13 +2,19 @@
 
 
 int main() {
-    .
+    int g = 0;
+    int ag[11];
+    ag[0] = 1;
+    ag[9] = 99;
+    ag[10] = 110;
+    int b=ag [0];
+    
     const char* lang = "C";
      printf("Hello and welcome to %s\n",  lang);
 
-    for (int i = 1; i <= 5; i++) {
+    for (int i = 0; i <= 10; i++) {
 
-         printf( "i = %i" , i  );
+         printf( "ag[%i] = %i\n" ,i, ag[i]   );
     }
 
     return 0;
