83f773c5dca1fcc58edb6e78972435dbbda652ed
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-05-05 21:45:54 +0300

    Переменные

--- main.c
+++ main.c
@@ -1,24 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h> <stdlib.h>
-void second (const int num, const char *sim  ) {
-    printf ("%s see you %i !!! \n",sim , num );
-}
-int main(const int argc, const char** argv) {
-    
-    const char* lang = "C";
-     printf("Hello and welcome to %s\n",  lang);
+#include <stdbool.h>
 
-    for (int i = 1; i <= 5; i++) {
+int main() {
+    // const short a = 0;
+    float x, y , res;
+    scanf("%f",&x);
+    scanf("%f",&y);
+    //long int
+    // print - Ld
+    res = x / y;
+    printf("Result : %.2f\n", res );
 
-         printf( "i = %i\n " , i  );
-    }
-second ( argc, argv[1]  );
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-    second ( argc, argv[1]  ) ;
-return 0;
-}
+    char s = 'S';
+    printf("%c\n", s);
+//    bool isHasCar = true;
+    // -32 768 - +32 768
+    //   short s = 6;
+    // 0 - +64 000
+    //unsigned short a = 5;
+    // -2 ml - +2 ml
+ //   int x = 6, y = 15, res;
+    //0 - +4ml
+ //   unsigned int a1 = 6;
+    //0 - +16000000
+//    long a2 = 5;
+//   unsigned long a3 = 6;
+    // 0 - +32 000 000
+ //   long long a4 = 5;
+    // 0 - +70 000 000
+//unsigned long long a5 = 6;
+    // для чисел с точкой и с деапозоном 9 чисел после точки
+//    float a6 = 4.5f;
+    //в два раза больше деапозон чем у float
+//    double a7 = 4.5f;
+    //lond double
+
+ //   char sym = 'f';
+
+    x += 5;
+
+    res = x + y;
+    res = x - y;
+    res = x / y;
+    printf("Variable: %d + %d = %d", x, y, res);
