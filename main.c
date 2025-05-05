be78617c6c5569a011009673ff0d776418b5ec67
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-05-05 21:15:52 +0300

    Переменые

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
+#include <stdlib.h> <stdlib.h>
 void second (const int num, const char *sim  ) {
     printf ("%s see you %i !!! \n",sim , num );
 }
-int main() {
+int main(const int argc, const char** argv) {
     
     const char* lang = "C";
      printf("Hello and welcome to %s\n",  lang);
@@ -12,6 +12,13 @@ int main() {
 
          printf( "i = %i\n " , i  );
     }
-second ( 1000000000," Vladimer "  ) ;
-
+second ( argc, argv[1]  );
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+    second ( argc, argv[1]  ) ;
+return 0;
 }
