54410f3be69156053f589063e27ac629df29c7cd
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-05-19 17:57:32 +0300

    main.c

--- main.c
+++ main.c
@@ -2,44 +2,63 @@
 #include <stdbool.h>
 
 int main() {
+
     // const short a = 0;
-    float x, y , res;
-    scanf("%f",&x);
-    scanf("%f",&y);
+   // float x =2025, y , res;
+    //scanf("%f",&x);
+    //&< это амперсанд
+    //scanf("%f",&y);
     //long int
     // print - Ld
-    res = x / y;
-    printf("Result : %.2f\n", res );
+    //res = x / y;
+   // printf("Result : %.2f\n", x );
+    //printf("Address Y: %u\n", &y );
 
-    char s = 'S';
-    printf("%c\n", s);
-//    bool isHasCar = true;
+    //float* ff = &x;
+    //float** ffp = &ff;
+
+   // char *xym_str;
+   // printf("Address X: %u\n", ff );
+    //printf('x = %f\n', ff) ;
+    //float z = *ff;
+    //printf(" Z: %f\n", *ff );
+   // printf("Z/ffp: %f\n", **ffp );
+    //char s = 'S';
+    //char * sym = &s;
+   // char* str = "Hello World";
+    //printf ("%s!\n", str );
+
+    //printf("%c\n", s);
+    //    bool isHasCar = true;
     // -32 768 - +32 768
     //   short s = 6;
     // 0 - +64 000
     //unsigned short a = 5;
     // -2 ml - +2 ml
- //   int x = 6, y = 15, res;
+    //   int x = 6, y = 15, res;
     //0 - +4ml
- //   unsigned int a1 = 6;
+      // unsigned int a1 = 6;
     //0 - +16000000
-//    long a2 = 5;
-//   unsigned long a3 = 6;
+    //    long a2 = 5;
+    //   unsigned long a3 = 6;
     // 0 - +32 000 000
- //   long long a4 = 5;
+    //   long long a4 = 5;
     // 0 - +70 000 000
-//unsigned long long a5 = 6;
+    //unsigned long long a5 = 6;
     // для чисел с точкой и с деапозоном 9 чисел после точки
-//    float a6 = 4.5f;
+    //    float a6 = 4.5f;
     //в два раза больше деапозон чем у float
-//    double a7 = 4.5f;
+    //    double a7 = 4.5f;
     //lond double
 
- //   char sym = 'f';
+    //char sym = 'f';
 
-    x += 5;
+    //x += 5;
 
-    res = x + y;
-    res = x - y;
-    res = x / y;
-    printf("Variable: %d + %d = %d", x, y, res);
+    //res = x + y;
+    //res = x - y;
+    //res = x / y;
+    //printf("Variable: %u ", a1);
+   // printf("Variable: %d + %d = %d", x, y, res);
+    return 0;
+}
