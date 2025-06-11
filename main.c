2e9f1ccf1c2c3219ed07dc7d26e9ca204e9b193b
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-06-11 16:30:34 +0300

    Изучение Масивов и Скаляров и Ввод строки , sizeof()

--- main.c
+++ main.c
@@ -1,13 +1,38 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 int main() {
 
     double eur ;
-    double usd = 4.2   ;
+    float usd  ;  // scalar
+    char str[50]; // array - pointer to address
+    char str1[50]; // array - pointer to address
+    char chr = 0; // = byte - 8 bit
+    /*int i_arr[10];
+    float f_arr[2];
+    float f_arr2[2];
+    double d_arr[2];
+    double d_arr2[2];
+    int size_c = sizeof(i_arr[2] ) ;
+    int size_f = sizeof(f_arr[2] ) ;
+    int size_d= sizeof(d_arr[2] ) ;*/
+
+    scanf("%s", str);
+    printf("str = %s\n", str);
+    size_t size = strlen(str);
+    for (int i = 0; i < size; i++) {
+
+        printf("str[%d] = %c\n", i, str[i]);
+    }
+
+    /*chr = str[0];
+     // strtol(str+1,&str,10);
     double rate = 1.12 ;
-     eur = usd / rate ;
-printf("Accepted usd: %f\n ",usd ) ;
-    printf("Accepted eur: %f\n ",eur) ;
-return 0; }
+    eur = usd / rate ;
+    printf("Accepted usd: %f\n ",usd ) ;
+    printf("Accepted eur: %f\n ",eur) ;*/
+    return 0;
+}
