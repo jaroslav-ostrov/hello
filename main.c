49ce41456cea175ba0e92fff842571cc813b6ef3
Jaroslav <ostrovskijaroslav815@gmail.com>
2025-06-27 17:26:26 +0300

    Memory viev

--- main.c
+++ main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 
-int main() {
+int main(int argc, char *argv[]) {
     int g = 0;
     int ag[11];
     ag[0] = 1;
     ag[9] = 99;
     ag[10] = 110;
     int b=ag [0];
-    
+
+    // стек используется для хранение переменных при вызове функций
+
     const char* lang = "C";
      printf("Hello and welcome to %s\n",  lang);
 
