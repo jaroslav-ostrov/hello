95c033e24e2e57c420de9cecc6b8a701944816bf
Oleg Taranenko MP <olegtaranenko@gmail.com>
2025-07-02 15:05:37 +0200

    fix errors

--- main.c
+++ main.c
@@ -2,27 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-/*int main(int argc, char *argv[]) {
-    int g = 0;
-    int ag[11];
-    ag[0] = 1;
-    ag[9] = 99;
-    ag[10] = 110;
-    int b=ag [0];
-
-    // стек используется для хранение переменных при вызове функций
-
-    const char* lang = "C";
-     printf("Hello and welcome to %s\n",  lang);
-
-    for (int i = 0; i <= 10; i++) {
-
-         printf( "ag[%i] = %i\n" ,i, ag[i]   );
-    }
-
-    return 0;
-
-} */
 void* append(short* data, size_t *length, size_t *capacity, short value)
 {
     if (*length >= *capacity) {
@@ -49,11 +28,11 @@ void* append(short* data, size_t *length, size_t *capacity, short value)
     (*length)++;
 
     return data ;
+}
 
 
 
-
-
+int main(int argc, char *argv[]) {
  /*char *ar = malloc(10) ; //NULL
     int* ptr_int = malloc (sizeof(int));
     short *ptr_short = malloc (7 * sizeof(short));
@@ -62,12 +41,12 @@ void* append(short* data, size_t *length, size_t *capacity, short value)
     free(ptr_short); */
    size_t capacity = 10;
     size_t length =0;
-    short *date = malloc(sizeof(short) * capacity);
+    short *data = malloc(sizeof(short) * capacity);
 
     for (int i = 0; i < 11; ++i) ;
-    date= append(data, *&length, *&capacity, rand () %40 - 20) ;
-     for (int i = 0; i < length; ++i) ;
-    printf("%d ", data[i]) ;
+    data= append(data, &length, &capacity, rand () %40 - 20) ;
+    for (int i = 0; i < length; ++i)
+        printf("%d ", data[i]) ;
     free(data);
 
 }
